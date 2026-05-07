7d6c1b762f7b9685800bdd4fd265e2ee108cc38a
Patrick Bucher <patrick.bucher@mailbox.org>
2026-05-07 09:30:48 +0200

    formatted code

--- typescript/scratch.ts
+++ typescript/scratch.ts
@@ -1,7 +1,24 @@
-type GameResult = { homeTeam: string, awayTeam: string, homeGoals: number, awayGoals: number, finished?: boolean };
-const first: GameResult = { homeTeam: "Manchester United", awayTeam: "Real Madrid", homeGoals: 3, awayGoals: 2 };
-const second : GameResult = { homeTeam: "Real Madrid", awayTeam: "Manchester United", homeGoals: 1, awayGoals: 1, finished: false };
-console.log(first, second)
+type GameResult = {
+  homeTeam: string;
+  awayTeam: string;
+  homeGoals: number;
+  awayGoals: number;
+  finished?: boolean;
+};
+const first: GameResult = {
+  homeTeam: "Manchester United",
+  awayTeam: "Real Madrid",
+  homeGoals: 3,
+  awayGoals: 2,
+};
+const second: GameResult = {
+  homeTeam: "Real Madrid",
+  awayTeam: "Manchester United",
+  homeGoals: 1,
+  awayGoals: 1,
+  finished: false,
+};
+console.log(first, second);
 
 type Output = string | number;
 let name: Output = "Patrick";
